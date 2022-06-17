9d50c3a6ca61a09013c3e7303960facfbed65499
Dean Long <17332032+dean-long@users.noreply.github.com>
2022-06-17 22:31:14 +0000

    Avoid "impossible encoding" for right-shift by 0

---
+++ test/hotspot/jtreg/compiler/codegen/ShiftByZero.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @bug 8288445
+ * @summary Test shift by 0
+ * @library /test/lib
+ * @run main compiler.codegen.ShiftByZero
+ * @run main/othervm -Xbatch -XX:-TieredCompilation
+ * -XX:+PrintOptoAssembly
+ * compiler.codegen.ShiftByZero
+ */
+
+package compiler.codegen;
+
+public class ShiftByZero {
+
+    public static final int N = 64;
+
+    public static int[] i32 = new int[N];
+
+    public static void bMeth() {
+        int shift = i32[0];
+        for (int i8 = 279; i8 > 1; --i8) {
+            shift <<= 6;
+        }
+        // low 6 bits of i1 are 0, so shift by i1 can be
+        // simplified to shift by 0
+        {
+            for (int i = 0; i < N; ++i) {
+                i32[i] += i32[i] >>= shift;
+            }
+            for (int i = 0; i < N; ++i) {
+                i32[i] += i32[i] >>>= shift;
+            }
+            for (int i = 0; i < N; ++i) {
+                i32[i] >>>= shift;
+            }
+            for (int i = 0; i < N; ++i) {
+                i32[i] >>= shift;
+            }
+            for (int i = 0; i < N; ++i) {
+                i32[i] <<= shift;
+            }
+        }
+    }
+
+    public static void main(String[] strArr) {
+        for (int i = 0; i < 20_000; i++ ) {
+            bMeth();
+        }
+    }
+}
