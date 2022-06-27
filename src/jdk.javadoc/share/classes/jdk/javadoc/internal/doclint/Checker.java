3d88d480920bdf4ca2799e8f9a2dc2289b3334bd
Pavel Rappo <prappo@openjdk.org>
2022-06-27 21:33:38 +0100

    feedback: make method positive

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclint/Checker.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclint/Checker.java
@@ -191,7 +191,7 @@ public class Checker extends DocTreePathScanner<Void, Void> {
                     reportMissing("dc.missing.comment");
                 }
                 return null;
-            } else if (tree.getFirstSentence().isEmpty() && !isOverridingMethod && notPseudoElement(p)) {
+            } else if (tree.getFirstSentence().isEmpty() && !isOverridingMethod && !pseudoElement(p)) {
                 if (tree.getBlockTags().isEmpty()) {
                     reportMissing("dc.empty.comment");
                     return null;
@@ -263,11 +263,11 @@ public class Checker extends DocTreePathScanner<Void, Void> {
         return null;
     }
 
-    // Checks if the passed tree path does NOT correspond to an entity, such as
+    // Checks if the passed tree path corresponds to an entity, such as
     // the overview file and doc-files/**/*.html files.
-    private boolean notPseudoElement(TreePath p) {
-        return p.getLeaf().getKind() != Tree.Kind.COMPILATION_UNIT
-                || p.getCompilationUnit().getSourceFile().getKind() != JavaFileObject.Kind.HTML;
+    private boolean pseudoElement(TreePath p) {
+        return p.getLeaf().getKind() == Tree.Kind.COMPILATION_UNIT
+                && p.getCompilationUnit().getSourceFile().getKind() == JavaFileObject.Kind.HTML;
     }
 
     private void reportMissing(String code, Object... args) {
