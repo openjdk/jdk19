70f83902a38e863b96fedb312982f7ac683ed068
Jorn Vernee <jbvernee@xs4all.nl>
2022-07-04 14:41:17 +0200

    Throw IAE when converting string with null byte.

--- src/java.base/share/classes/java/lang/foreign/SegmentAllocator.java
+++ src/java.base/share/classes/java/lang/foreign/SegmentAllocator.java
@@ -76,6 +76,7 @@ public interface SegmentAllocator {
      * into a new memory segment obtained by calling {@code this.allocate(str.length() + 1)}.
      * @param str the Java string to be converted into a C string.
      * @return a new native memory segment containing the converted C string.
+     * @throws IllegalArgumentException if the UTF-8 encoded bytes of the given string contain a {@code 0} byte.
      */
     default MemorySegment allocateUtf8String(String str) {
         Objects.requireNonNull(str);
